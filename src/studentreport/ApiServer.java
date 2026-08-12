03a728833fb9ca1c8f2bbef18a71703b7c738169
ThiliniYA <thiliniyapa88@gmail.com>
2026-08-12 15:16:57 +1200

    Add /students endpoint that returns student data from database as JSON

--- src/studentreport/ApiServer.java
+++ src/studentreport/ApiServer.java
@@ -2,6 +2,7 @@ package studentreport;
 
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 
 public class ApiServer {
 
@@ -9,8 +10,27 @@ public class ApiServer {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(7000), 0);
 
-        server.createContext("/hello", exchange -> {
-            String response = "Hello from the Student Report API!";
+       server.createContext("/students", exchange -> {
+
+            ArrayList<Student> students = Database.loadAllStudents();
+
+            StringBuilder json = new StringBuilder();
+            json.append("[");
+
+            for (int i = 0; i < students.size(); i++) {
+                Student s = students.get(i);
+                json.append("{\"id\":\"").append(s.getStudentID()).append("\",");
+                json.append("\"name\":\"").append(s.getFullName()).append("\"}");
+
+                if (i < students.size() - 1) {
+                    json.append(",");
+                }
+            }
+
+            json.append("]");
+
+            String response = json.toString();
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
             exchange.sendResponseHeaders(200, response.length());
             exchange.getResponseBody().write(response.getBytes());
             exchange.close();
